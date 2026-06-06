5281a9db6c2be0ae7bbeff1b2f0020a24ddfb11e
Harshul Jain <harshuljain2047@gmail.com>
2026-06-07 00:35:34 +0530

    Add: squares-of-a-sorted-array - submission-0

---
+++ Data Structures & Algorithms/squares-of-a-sorted-array/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+      int n = nums.size();
+      vector<int> sortedSquareNums(n);
+      int i = 0, j = n-1;
+      int k = n-1;
+      while(k >= 0) {
+        if(abs(nums[i]) > abs(nums[j])) {
+          sortedSquareNums[k] = nums[i]*nums[i];
+          i++;
+          k--;
+        } else {
+          sortedSquareNums[k] = nums[j]*nums[j];
+          j--;
+          k--;
+        }
+      }
+      return sortedSquareNums;
+    }
+};
